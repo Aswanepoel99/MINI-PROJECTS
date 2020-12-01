199be43197c60a3d4790bce1212215c803ba76a2
Aswanepoel99 <andreotters4@gmail.com>
2020-12-01 06:48:38 +0200

    commit progress

--- html and css dec 1 2020/app.js
+++ html and css dec 1 2020/app.js
@@ -1,6 +1,5 @@
 const navButton = document.getElementById("openNav")
 
-
 /* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -11,6 +10,3 @@ function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
 navButton.addEventListener('click', openNav)
-
-console.dir(navButton)
-console.dir(document.getElementById("mySidenav"))
